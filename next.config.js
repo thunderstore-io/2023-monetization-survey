28ea9a0948100e34ed7249363d4595285d0dace0
Mythic <mythic@thunderstore.io>
2023-06-07 06:28:10 +0300

    Configure assetPrefix for GitHub pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  assetPrefix: isProd ? "/2023-monetization-survey" : "",
   experimental: {
     appDir: true,
   },
